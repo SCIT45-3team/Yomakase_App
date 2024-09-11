6804470d09bd21527dc14861046536af17ace731
choiyooon <cyooon>
2024-09-11 11:19:35 +0900

    Initial Commit

---
+++ Yomakase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yomakase
+//
+//  Created by 최윤석 on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
